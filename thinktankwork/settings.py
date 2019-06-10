280914415e1b3ab79720b747978564700c1581f3
William Gunnells <gunnells@gmail.com>
2019-06-09 18:43:12 -0700

    dj_database

--- thinktankwork/settings.py
+++ thinktankwork/settings.py
@@ -86,13 +86,14 @@ WSGI_APPLICATION = 'thinktankwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
+DATABASES={'default':dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
