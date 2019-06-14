7096c856ad61fe90765fbd7a32c086a434e94a0b
William Gunnells <wgunnells@box.com>
2019-06-13 20:07:42 -0700

    frontpage stuff

--- basicapp/views.py
+++ basicapp/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render
 
 from django.shortcuts import render_to_response
 
+
 def index(request):
 	return render_to_response("index.html")
+
+
+def calendar(request):
+	return render_to_response("calendar.html")
