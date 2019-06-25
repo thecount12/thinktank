7fdda66340b81b99518e69ea010e1f9f24618e67
William Gunnells <wgunnells@box.com>
2019-06-24 18:54:35 -0700

    changed membership and resources

--- member/models.py
+++ member/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class MemberPackage(models.Model):
     package_name = models.CharField(max_length=60)
-    resource = models.CharField(max_length=60)  # dedicated desk, shared_space, day_use
+    # resource = models.CharField(max_length=60)  # dedicated desk, shared_space, day_use
+    resource = models.ForeignKey('member.MemberResources', on_delete=models.DO_NOTHING)
     monthly_rate = models.DecimalField(decimal_places=2, max_digits=10, blank=True, null=True)
     overage_rate = models.DecimalField(decimal_places=2, max_digits=10, blank=True, null=True)
     Semi_annual = models.DecimalField(decimal_places=2, max_digits=10,  blank=True, null=True)
@@ -14,7 +15,22 @@ class MemberPackage(models.Model):
 
 
 class Membership(models.Model):
-    member = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
-    bill_day = models.IntegerField(max_length=2, blank=True, null=True)
+    member_name = models.ForeignKey('auth.User', related_name="member_name", on_delete=models.DO_NOTHING)
+    bill_day = models.IntegerField(blank=True, null=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
+    package_name = models.CharField(max_length=60)
+    paid_by = models.ForeignKey('auth.User', related_name='member_paid', on_delete=models.DO_NOTHING)
+
+    def __unicode__(self):
+        return self.member_name
+
+
+class MemberResources(models.Model):
+    resource_name = models.CharField(max_length=60)  # dedicated desk,shared_space,day_use,key,locker,mail
+    resource_start = models.DateField(null=True, blank=True)
+    resource_end = models.DateField(null=True, blank=True)
+    resource_fee = models.DecimalField(decimal_places=2, max_digits=10,  blank=True, null=True)
+
+    def __unicode__(self):
+        return self.resource_name
