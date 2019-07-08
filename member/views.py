adc29d61c58101a7ad3529fe0b3bc30310427a7b
William Gunnells <gunnells@gmail.com>
2019-07-07 18:47:30 -0700

    member page stuff and proper authentication

--- member/views.py
+++ member/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.shortcuts import render_to_response
 
 
+@login_required
 def member_index(request):
-    return render_to_response("member.html")
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+        print(username)
+        data = {'user': username}
+    # return render_to_response("member.html")
+    return render(request, "member.html", data)
