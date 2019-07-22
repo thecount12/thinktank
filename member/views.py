1cf3f9b5e3a5bdbb06a207ff9992bbde9849c45e
William Gunnells <gunnells@gmail.com>
2019-07-22 09:49:19 -0700

    more flow stuff

--- member/views.py
+++ member/views.py
@@ -23,7 +23,9 @@ def member_index(request):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
+        user_id = request.user.id
         print(username)
-        data = {'user': username}
+        print(request.user.id)
+        data = {'user': username, 'user_id': user_id}
     # return render_to_response("member.html")
     return render(request, "member.html", data)
